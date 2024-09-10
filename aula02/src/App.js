0d69f02b55d6fc2df72a6b0675ebaf29a16d8061
Marcos Andrade <mvpdeandrade@gmail.com>
2024-09-10 15:23:40 -0300

    Renderizando Listas

--- aula02/src/App.js
+++ aula02/src/App.js
@@ -2,10 +2,26 @@ import logo from './logo.svg';
 import './App.css';
 import Loja from './components/Loja';
 
+const textos = [
+  {id: 1, texto: 'Texto 1'},
+  {id: 2, texto: 'Texto 2'},
+  {id: 3, texto: 'Texto 3'},
+  {id: 4, texto: 'Texto 4'},
+  {id: 5, texto: 'Texto 5'},
+];
+
+function Item({item}){
+  return <li>{item.texto}</li>
+}
+
 function App() {
   return (
     <div>
-      <Loja />
+      <ul>
+        {
+          textos.map(x => <Item key={x.id} item={x}/>)
+        }
+      </ul>
     </div>
   );
 }
